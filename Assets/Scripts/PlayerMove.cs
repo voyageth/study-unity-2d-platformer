91440816754fe5188af7cacd4e5e70678756e3b5
voyageth <voyageth@gmail.com>
2024-05-05 00:12:02 +0900

    :rocket: 프로젝트 준비, 아틀라스와 애니메이션, 플레이어 이동 구현

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float maxSpeed;
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        // 속도 decay
+        if (Input.GetButtonUp("Horizontal"))
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+
+        // 방향 전환
+        if (Input.GetButtonDown("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+
+        // Animation
+        if (Mathf.Abs(rigid.velocity.x) < 0.3)
+            animator.SetBool("isWalking", false);
+        else
+            animator.SetBool("isWalking", true);
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+        
+        if (rigid.velocity.x > maxSpeed)
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        else if (rigid.velocity.x < -maxSpeed)
+            rigid.velocity = new Vector2(-maxSpeed, rigid.velocity.y);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# study-unity-2d-platformer
+# unity 2D 플랫포머 게임 만들어 보기
+- 유튜브 강좌 보고 따라해 보자.
+
+# 출처
+- [BE2 강좌 재생목록](https://www.youtube.com/playlist?list=PLO-mt5Iu5TeZGR_y6mHmTWyo0RyGgO0N_)
+- [골드메탈 스튜디오](https://www.goldmetal.co.kr)
+
+# unity editor 에서 visual studio 연결
+- https://learn.microsoft.com/en-us/visualstudio/gamedev/unity/get-started/getting-started-with-visual-studio-tools-for-unity?pivots=windows#configure-unity-to-use-visual-studio
+- https://learn.microsoft.com/en-us/visualstudio/gamedev/unity/get-started/using-visual-studio-tools-for-unity?pivots=windows
